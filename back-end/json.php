930736eca6fdf65d4a1f8d402d131a61f81fc461
Nouman Sarwar <noumansarwar63@hotmail.com>
2023-05-23 11:19:11 -0700

    :sparkles: feat: add a new student feature has been completed

--- back-end/json.php
+++ back-end/json.php
@@ -1,6 +1,28 @@
 <?php
 
-function addStudentToFileJSON(): void
+/**
+ * Add new student and stores into the json file
+ *
+ * @param string $fileName
+ * @param int $registrationNumber
+ * @param string $name
+ * @param int $grade
+ * @param string $classroom
+ * @return void
+ */
+function addStudentToFileJSON(string $fileName, int $registrationNumber, string $name, int $grade, string $classroom): void
 {
+    if (is_writable($fileName)) {
+        $tasks = json_decode(file_get_contents($fileName), true);
+        $tasks[] = [
+            'registrationNumber' => $registrationNumber,
+            'name' => $name,
+            'grade' => $grade,
+            'classroom' => $classroom,
+        ];
 
+        if(!file_put_contents($fileName, json_encode($tasks))){
+            echo "Cannot write to the file!";
+        }
+    }
 }
