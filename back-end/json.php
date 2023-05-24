d9402a44465468288ebe87bf7e1bb86046bdf56f
Nouman Sarwar <noumansarwar63@hotmail.com>
2023-05-24 08:57:07 -0700

    :recycle: refactor: rename array $tasks into $students

--- back-end/json.php
+++ back-end/json.php
@@ -13,15 +13,15 @@
 function addStudentToFileJSON(string $fileName, int $registrationNumber, string $name, int $grade, string $classroom): void
 {
     if (is_writable($fileName)) {
-        $tasks = json_decode(file_get_contents($fileName), true);
-        $tasks[] = [
+        $students = json_decode(file_get_contents($fileName), true);
+        $students[] = [
             'registrationNumber' => $registrationNumber,
             'name' => $name,
             'grade' => $grade,
             'classroom' => $classroom,
         ];
 
-        if(!file_put_contents($fileName, json_encode($tasks))){
+        if(!file_put_contents($fileName, json_encode($students))){
             echo "Cannot write to the file!";
         }
     }
